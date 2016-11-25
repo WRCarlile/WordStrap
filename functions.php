0b0eace0b1a46fac6285e5bc7ca5ae14606f018e
“Ryan <ryancarlile@gmail.com>
2016-11-25 13:56:43 -0800

    Set up navwalker nav bar

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+require_once('wp_bootstrap_navwalker.php');
+function theme_setup() {
+	// Featured Image Support
+	add_theme_support('post-thumbnails');
+
+	register_nav_menus( array(
+    'primary' => __( 'Primary Menu'),
+) );
+
+
+}
+add_action('after_setup_theme', 'theme_setup');
+
+
+
+
+
+
+
+
+ ?>
